e222c823f777145cce0e05afa335fc7ed42cce0b
sethoak <sethoak@gmail.com>
2019-09-16 09:27:07 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lightining Exercise
+
+Lightning Exercise 1: Create an object that represents a bill from your doctor's office. It should have the following properties: officeName, streetAddress, doctorName, patientName, visitDate, amountBilled, dueDate. You provide example values for each one.
+# doctorBill
---
+++ js/main.js
@@ -0,0 +1,14 @@
+const bill = {
+  officeName: "Bills Dr.",
+  streetAddress: "301 Park Plus",
+  patientName: "Seth",
+  visitDate: "09/16/19",
+  amountBilled: "$100.00",
+  dueDate: "09/18/19"
+};
+
+const dateVisted = bill["visitDate"];
+const owed = bill["amountBilled"];
+const patient = bill["patientName"];
+
+console.log(dateVisited);
